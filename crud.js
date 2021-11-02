bcaed110c42bc034cb9e930391161e1a38abf328
unknown <claudiaclrduro@gmail.com>
2021-11-02 11:27:29 +0100

    guardado por si acaso

--- crud.js
+++ crud.js
@@ -1,10 +1,20 @@
 let cajaTexto = document.getElementById("userInput");
 let boton = document.getElementById("userButton");
-let listatarea = document.getElementById("list");
+let listaTarea = document.getElementById("list");
+let delete = document.getElementById("");
 
+class ListElement = {
+    constructor(id, text){
+        this.id = id;
+        this.text = text;
+    }
 
-function create(){
-    let tarea = cajaTexto.value;
-    listatarea.innerHTML += `${tarea}`;
 }
 
+function create(){
+    //let tarea = cajaTexto.value;
+    let ListElement = new ListElement(id, text); 
+    listaTarea.innerHTML += `<li>${tarea}</li> <button> delete </button>`;
+}
+
+boton.addEventListener('click', create);
